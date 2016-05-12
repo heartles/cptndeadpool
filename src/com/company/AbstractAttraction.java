659f61390e008b1b400658baeada947931cdf2b4
Justin Mellott <jbmdev@outlook.com>
2016-05-12 14:54:07 -0700

    Removed workspace from repository
    Also added members to AbstractAttraction class

--- src/com/company/AbstractAttraction.java
+++ src/com/company/AbstractAttraction.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Justin on 5/12/2016.
  */
 public abstract class AbstractAttraction {
+    public String getName() {
+        return name;
+    }
+
+    String name;
+
+    protected AbstractAttraction()
+    {
+        name = "Tester attraction";
+    }
 }
