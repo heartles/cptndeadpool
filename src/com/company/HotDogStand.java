962d14997c16b9d357081f9296470b0e47cb6ad3
macgm120 <macgm120@hsd.k12.or.us>
2016-05-16 15:20:22 -0700

    Add HotDogStand as experiment in restaurant

---
+++ src/com/company/HotDogStand.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.*;
+
+/**
+ * Created by macgm120 on 5/16/2016.
+ */
+public class HotDogStand extends AbstractRestaurant{
+    public HotDogStand(){}
+    public int sellItem(String foodSold){
+        int result=0;
+        for(int i=0; i<menu.size(); i++){
+            if(foodSold.equals(menu.get(i).foodName)){
+                result= (menu.get(i).foodPrice-menu.get(i).foodCost);
+                break;
+            }
+        }
+       return result;
+    }
+}
