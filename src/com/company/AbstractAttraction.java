6a33bd96e903796e10252f747a26884ce19c6f24
macgm120 <macgm120@hsd.k12.or.us>
2016-05-16 14:13:09 -0700

    Added height to AbAttract and added FerrisWheel

--- src/com/company/AbstractAttraction.java
+++ src/com/company/AbstractAttraction.java
@@ -11,8 +11,16 @@ public abstract class AbstractAttraction {
     }
     public int getHeight() {return height;}
 
+    int profitPerRider;
     String name;
     int height;
+
+    /**
+     * @param riders the number of riders
+     * @return the profit of riding the ride
+     */
+
+    abstract protected int rideAttraction(int riders);
 
     protected AbstractAttraction()
     {
--- src/com/company/MiniGolf.java
+++ src/com/company/MiniGolf.java
@@ -4,17 +4,21 @@ package com.company;
  * Created by macgm120 on 5/12/2016.
  */
 public class MiniGolf extends AbstractAttraction {
-    private int holes;
-    private int diff;
-    public MiniGolf(int numHoles, int newsDifficulty, String newCourseName) {
+    private int holes;//number of holes in the course
+    private int diff;//course difficulty on a 1-10 scale
+    public MiniGolf(int numHoles, int newsDifficulty, String newCourseName, int newProfit) {
         holes = numHoles;
         diff = newsDifficulty;
         name = newCourseName;
+        profitPerRider=newProfit;
     }
     public void renovateCourse(int numHoles, int newsDifficulty, String newCourseName){
         holes = numHoles;
         diff = newsDifficulty;
         name = newCourseName;
+    }
+    protected int rideAttraction(int riders){
+        return riders*profitPerRider;
     }
 
 }
