69987ba925d1af8f5bd2abc3194f88b90c121831
macgm120 <macgm120@hsd.k12.or.us>
2016-05-18 15:04:43 -0700

    Changes to AbstractRestaurant

--- src/com/company/HotDogStand.java
+++ src/com/company/HotDogStand.java
@@ -6,14 +6,5 @@ import java.util.*;
  */
 public class HotDogStand extends AbstractRestaurant{
     public HotDogStand(){}
-    public int sellItem(String foodSold){
-        int result=0;
-        for(int i=0; i<menu.size(); i++){
-            if(foodSold.equals(menu.get(i).foodName)){
-                result= (menu.get(i).foodPrice-menu.get(i).foodCost);
-                break;
-            }
-        }
-       return result;
-    }
+    
 }
