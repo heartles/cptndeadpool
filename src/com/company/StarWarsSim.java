e38a170424aa719ac26f4f8c041554d9895e941f
CALV1N L33 <abstergo.calvinstark@gmail.com>
2016-06-06 14:31:02 -0700

    Finished StarWarsSim

--- src/com/company/StarWarsSim.java
+++ src/com/company/StarWarsSim.java
@@ -9,26 +9,31 @@ public class StarWarsSim extends AbstractAttraction
     private String scene; //Name of the specific simulation
     private int movement; //Ranks movement of simulator, 1-5
     private int maxPatrons;
+    String Response;
     public StarWarsSim(String newName, double newCost, double newProfit,int newHeight, int seats, String sceneName,int newMovement)
     {
+        BigDecimal A = new BigDecimal(newCost);
+        BigDecimal B = new BigDecimal(newProfit);
         movement = newMovement;
         scene = sceneName;
         maxPatrons = seats;
         height = newHeight;
         name = newName;
-        costToRun = BigDecimal.valueOf(newCost).round(new MathContext(String.valueOf(((int) newCost))+2));
-        profitPerRider = BigDecimal.valueOf(newProfit).round(new MathContext(String.valueOf(((int) newProfit))+2));
-    }
+        costToRun = A;
+        profitPerRider = B;
 
-    public void Reaction(){
-        int Move = movement;
-        if (Move > 3)
-            System.out.println("This ride is for the common man.");
-        else if (Move == 5)
-            System.out.println("This ride will kill you.");
-        else if (Move < 3)
-            System.out.println("This ride is for wimps.");
-}
+
+        if (movement > 3)
+            Response = ("This ride is for the common man. Movement value is " + movement);
+        else if (movement == 5)
+            Response =  "This ride will kill you. Movement value is " + movement;
+        else if (movement < 3)
+            Response = "This ride is for wimps. Movement value is " + movement;
+    }
+    public String SafetyCheck()
+    {
+        return Response;
+    }
     public String Scene(){
         return "Welcome to " + scene;
     }
