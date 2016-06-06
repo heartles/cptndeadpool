80deaccf219945a6173fb505758cb9c7a7a6792b
CALV1N L33 <abstergo.calvinstark@gmail.com>
2016-06-06 09:10:03 -0700

    Added StarWarsSim

--- src/com/company/StarWarsSim.java
+++ src/com/company/StarWarsSim.java
@@ -1,19 +1,42 @@
 package com.company;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 //Calvin Lee
 public class StarWarsSim extends AbstractAttraction
 {
     private String scene; //Name of the specific simulation
     private int movement; //Ranks movement of simulator, 1-5
-    public StarWarsSim(String newName, int newCost, int newProfit,int newHeight)
+    private int maxPatrons;
+    public StarWarsSim(String newName, double newCost, double newProfit,int newHeight, int seats, String sceneName,int newMovement)
     {
+        movement = newMovement;
+        scene = sceneName;
+        maxPatrons = seats;
         height = newHeight;
         name = newName;
-        costToRun = newCost;
-        profitPerRider = newProfit;
-
+        costToRun = BigDecimal.valueOf(newCost).round(new MathContext(String.valueOf(((int) newCost))+2));
+        profitPerRider = BigDecimal.valueOf(newProfit).round(new MathContext(String.valueOf(((int) newProfit))+2));
     }
 
-    protected int rideAttraction(int riders){
-        return (riders*profitPerRider)-costToRun;
+    public void Reaction(){
+        int Move = movement;
+        if (Move > 3)
+            System.out.println("This ride is for the common man.");
+        else if (Move == 5)
+            System.out.println("This ride will kill you.");
+        else if (Move < 3)
+            System.out.println("This ride is for wimps.");
+}
+    public String Scene(){
+        return "Welcome to " + scene;
     }
+
+    protected BigDecimal rideAttraction(int riders) {
+        BigDecimal Results = profitPerRider.multiply(BigDecimal.valueOf(riders)).subtract(costToRun);
+        return Results;
+    }
+
+
 }
